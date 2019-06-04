31a900653cbbb1b82f30b932003f1cc779a38e20
gerardo <gerardnrmtz@gmail.com>
2019-06-03 21:55:07 -0500

    modificacion de la la url de la api

--- src/Components/ForecastExtended.js
+++ src/Components/ForecastExtended.js
@@ -18,7 +18,7 @@ const data={
     wind:'normal'
 }
 export const api_key="898a8db3af3a78272cfe37a07f3312cb";
-export const url_base_weather="http://api.openweathermap.org/data/2.5/forecast";
+export const url_base_weather="https://api.openweathermap.org/data/2.5/forecast";
 
 
 class ForecastExtended extends Component
